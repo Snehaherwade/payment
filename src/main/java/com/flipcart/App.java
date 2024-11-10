64e190fe59bc2d332c22dbdca3851833a79899ba
Snehaherwade <sneha.herwade25@gmail.com>
2024-11-10 11:55:10 +0530

    BuildProject2

---
+++ src/main/java/com/flipcart/App.java
@@ -0,0 +1,14 @@
+package com.flipcart;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int sum,a=10,b=20;
+        System.out.println("Hello World!");
+        sum=a+b;
+        System.out.println("sum : "+sum);
+
+    }
+}
